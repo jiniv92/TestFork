533760ff7dbb1af5ccce3cd0639bea0979acba86
sjsam <sjsam@tangledweb.in>
2019-10-07 23:50:14 +0530

    Fixed big syntax error

--- Driver.cpp
+++ Driver.cpp
@@ -4,7 +4,7 @@ using std::endl;
 auto main(int argc,char* argv[]) -> int
 {
   // Printing the important message
-  cout<<Hello Wild World !"<<endl;
+  cout<<"Hello Wild World !"<<endl;
   // Returning zero on success policy
   return 0;
 }
