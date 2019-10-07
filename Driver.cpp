5269eba09c89a71a393ba0384d4c8d7c74c35605
jiniv92 <jini.varghese@outlook.com>
2019-10-08 00:43:33 +0530

    Styling

--- Driver.cpp
+++ Driver.cpp
@@ -4,7 +4,9 @@ using std::endl;
 auto main(int argc,char* argv[]) -> int
 {
   // Printing the important message
+  cout<<"------------------"<<endl;
   cout<<"Hello Wild World !"<<endl;
+  coyt<<"------------------"<<endl;
   // Returning zero on success policy
   return 0;
 }
